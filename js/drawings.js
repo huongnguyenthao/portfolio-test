ca024f1b4652bb5e18718ef2d384c3f9daadaacf
huongnguyenthao <hnguyen16@cornellcollege.edu>
2016-11-06 00:31:31 -0400

    Added navbar

--- js/drawings.js
+++ js/drawings.js
@@ -60,3 +60,12 @@ $(window).resize(function() {
 	
 });
 
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
